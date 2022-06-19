28e4e2eee31c2e7ac6830484d5d320ad97a7ff14
bharath <bharathvj98@gmail.com>
2022-06-19 22:40:48 +0530

    first commit

---
+++ src/com/oopsAssignment2/driver/Driver.java
@@ -0,0 +1,63 @@
+package com.oopsAssignment2.driver;
+
+import java.util.Scanner;
+
+import com.oopsAssignment2.model.Employee;
+import com.oopsAssignment2.service.CredentialService;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Employee employee = new Employee("Barath", "Hitman");// TODO Auto-generated method stub
+		CredentialService cs = new CredentialService();
+		String generatedEmail;
+		char[]generatedPassword;
+		System.out.println("please enter the department from the following");
+		System.out.println("1 Technial");
+		System.out.println("2. Admin");
+		System.out.println("3. Human Resource");
+		System.out.println("4. legal");
+		
+		Scanner sc =new Scanner(System.in);
+		int option = sc.nextInt();
+		switch (option){
+		case (1):
+		generatedEmail=cs.generateEmailAddress(employee.getFirstName().toLowerCase(),employee.getLastName().toLowerCase(),"Tech");
+			
+		generatedPassword=cs.generatePassword();
+		
+			cs.showCredentials(employee,generatedEmail,generatedPassword);
+			break;
+			case (2):
+				generatedEmail=cs.generateEmailAddress(employee.getFirstName().toLowerCase(),employee.getLastName().toLowerCase(),"ADM");
+			
+			generatedPassword=cs.generatePassword();
+			
+			cs.showCredentials(employee,generatedEmail,generatedPassword);
+			
+			
+			break;
+			case (3):
+				generatedEmail=cs.generateEmailAddress(employee.getFirstName().toLowerCase(),employee.getLastName().toLowerCase(),"HR");
+			
+			generatedPassword=cs.generatePassword();
+			
+			cs.showCredentials(employee,generatedEmail,generatedPassword);
+	
+	
+			break;
+			case (4):
+		
+				generatedEmail=cs.generateEmailAddress(employee.getFirstName().toLowerCase(),employee.getLastName().toLowerCase(),"legal");
+			
+			generatedPassword=cs.generatePassword();
+					cs.showCredentials(employee,generatedEmail,generatedPassword);
+		
+			break;
+			default:
+					System.out.println("please enter correct inputs");
+			break;
+		}
+	}
+
+}
---
+++ src/com/oopsAssignment2/service/CredentialService.java
@@ -0,0 +1,31 @@
+package com.oopsAssignment2.service;
+
+import java.util.Random;
+
+import com.oopsAssignment2.model.Employee;
+
+public class CredentialService {
+public String generateEmailAddress (String firstName, String lastName, String department){
+	return firstName+lastName+"@"+department+"universe.com";
+}
+public char[] generatePassword() {
+	String cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	String small = "acdefghijklmnopqrstuvwxyz";
+	String num = "0123456789";
+	String spec = "!@#$%^&*";
+			String values = cap+small+num+spec;
+	Random random = new Random();
+	char[] password = new char[9];
+	for (int i=0; i<9; i++) {
+		password[i] = values.charAt(random.nextInt(values.length()));
+	}
+	
+	return password;
+}
+public void showCredentials(Employee employee, String generatedEmail, char[] generatedPassword) {
+	System.out.println("your generated credentials are");
+	System.out.println(generatedEmail);
+	System.out.println(generatedPassword);
+
+}
+}
